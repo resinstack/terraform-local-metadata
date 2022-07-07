0258dfda47563480bec2c9544d29f0c03eb36e57
Michael Aldridge <aldridge.mac@gmail.com>
2022-07-07 00:47:05 -0500

    Remove vault emissary config

--- tpl/vault.tpl
+++
@@ -1,9 +0,0 @@
----
-dest: /run/config/vault/85-gen-secret.hcl
-mode: 0400
-onrender: /usr/bin/sv restart vault
----
-storage "consul" {
-  token        = "{{poll "${secret_provider}" "${vault_consul_token_name}"}}"
-  service_tags = "${service_tags}"
-}
--- vault.tf
+++ vault.tf
@@ -7,17 +7,3 @@ resource "local_file" "vault_kms_id" {
   file_permission      = "0644"
   directory_permission = "0755"
 }
-
-resource "local_file" "emissary_vault" {
-  count = var.vault_server ? 1 : 0
-
-  content = templatefile("${path.module}/tpl/vault.tpl", {
-    secret_provider         = var.secret_provider
-    service_tags            = var.vault_service_tags
-    vault_consul_token_name = "resinstack-vault-consul-token-${var.cluster_tag}"
-  })
-
-  filename             = "${var.base_path}/emissary/90-gen-vault.tpl"
-  file_permission      = "0644"
-  directory_permission = "0755"
-}
